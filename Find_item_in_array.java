1a0ea9174166ec3abd041521821ebd286f8aa215
yash <yash@gmail.com>
2024-08-28 16:14:18 +0530

    Second Change

--- Find_item_in_array.java
+++ Find_item_in_array.java
@@ -1,7 +1,23 @@
-
+import java.util.*;
 public class Find_item_in_array
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int[] arr = { 2,3,4,5,3,5,7};
+		Find(arr,5,0);
+		System.out.println(List);
+	}
+	static ArrayList<Integer> List = new ArrayList<>();
+	static void Find(int[] arr,int search, int index){
+		if(index == arr.length){
+			return ;
+		}
+		if(arr[index] == search){
+		
+			List.add(index);
+			
+
+		}
+		 Find(arr,search,index + 1);
+
 	}
 }
